ba9ccb3b9877bbe44bc35d574cab9b2e2751664c
xiongchenchen <xiongchenchen@wondersgroup.com>
2021-09-26 10:43:11 +0800

    删除链表倒数第n个节点

---
+++ src/offerII/Offer021.java
@@ -0,0 +1,33 @@
+package offerII;
+
+/**
+ * 删除链表倒数第n个节点
+ */
+public class Offer021 {
+
+    /**
+     * 快慢指针
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null || n < 1) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        if (fast == null) {
+            return head.next;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
