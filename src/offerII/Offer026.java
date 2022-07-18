63e812caa24f041f8152128b7270ad420e77aed6
xiongchenchen <1443682928@qq.com>
2022-07-18 10:32:27 +0800

    重排链表

---
+++ src/offerII/Offer026.java
@@ -0,0 +1,34 @@
+package offerII;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 重排链表
+ */
+public class Offer026 {
+
+    /**
+     * 线性表
+     * @param head
+     */
+    public void reorderList(ListNode head) {
+        List<ListNode> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head);
+            head = head.next;
+        }
+        int i = 0, j = list.size() - 1;
+        while (i < j) {
+            list.get(i).next = list.get(j);
+            i++;
+            if (i == j) {
+                break;
+            }
+            list.get(j).next = list.get(i);
+            j--;
+        }
+        list.get(i).next = null;
+    }
+
+}
