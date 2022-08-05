d7d66b9d5ec953482de533b4df77bdcf12dc10b6
xiongchenchen <1443682928@qq.com>
2022-08-05 10:29:26 +0800

    在二叉树中增加一行

---
+++ src/day/day623.java
@@ -0,0 +1,31 @@
+package day;
+
+/**
+ * 在二叉树中增加一行
+ */
+public class day623 {
+
+    /**
+     * 递归
+     * @param root
+     * @param val
+     * @param depth
+     * @return
+     */
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if (root == null) {
+            return null;
+        }
+        if (depth == 1) {
+            return new TreeNode(val, root, null);
+        }
+        if (depth == 2) {
+            root.left = new TreeNode(val, root.left, null);
+            root.right = new TreeNode(val, null, root.right);
+        } else {
+            root.left = addOneRow(root.left, val, depth - 1);
+            root.right = addOneRow(root.right, val, depth - 1);
+        }
+        return root;
+    }
+}
